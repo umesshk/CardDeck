d1ba1eb35a2af60f8430f33e96511ad593c29ab9
umesshk <umeshkumar01439@gmail.com>
2026-06-19 09:31:25 +0530

    card struct

---
+++ deck/card.go
@@ -0,0 +1,35 @@
+package deck
+
+type Suit uint8
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+
+type Rank uint8
+
+const (
+	_ Rank = iota
+	Ace
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+type Card struct {
+	Suit
+	Rank
+}
